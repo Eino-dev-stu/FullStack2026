01c451dd1bc80a748639e25e7b31fbf229d1147d
Eino-dev-stu <juhan.pg@gmail.com>
2026-09-07 14:27:11 +0300

    1.4

--- part1/kurssitiedot/src/App.jsx
+++ part1/kurssitiedot/src/App.jsx
@@ -2,11 +2,12 @@ const Header = (props) => {
   return <h1>{props.course}</h1>
 }
 const Content = (props) => {
+  console.log(props, "here2")
   return (
     <div>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   )
 }
@@ -19,12 +20,14 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
-  console.log(props.part1.exercises, "here")
+  console.log(props, "here")
   return (
     <div>
       <p>
         number of exercises{" "}
-        {props.part1.exercises + props.part2.exercises + props.part3.exercises}
+        {props.parts[0].exercises +
+          props.parts[1].exercises +
+          props.parts[2].exercises}
       </p>
     </div>
   )
@@ -50,8 +53,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={parts[0]} part2={parts[1]} part3={parts[2]} />
-      <Total part1={parts[0]} part2={parts[1]} part3={parts[2]} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
